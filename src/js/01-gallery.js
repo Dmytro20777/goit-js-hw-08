de14736fe469ea06e134616831c6888c733c5140
Dmytro20777 <08dmutro11@gmail.com>
2023-11-18 09:59:16 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,12 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-import { galleryItems } from './gallery-items.js';
 
 const listGallery = document.querySelector('.gallery');
 let instance;
@@ -16,11 +15,10 @@ function createListGallery(array) {
   return array
     .map(({ preview, description, original }) => 
       `<li class="gallery__item">
-         <a class="gallery__link" href="large-image.jpg">
+         <a class="gallery__link" href="${original}">
            <img
              class="gallery__image"
              src="${preview}"
-             data-source="${original}"
              alt="${description}"
            />
          </a>
@@ -36,25 +34,77 @@ function handleClick(event) {
 
   const target = event.target;
   if (target.classList.contains('gallery__image')) {
-    const largeImageUrl = target.dataset.source;
+    const index = galleryItems.findIndex(item => item.original === target.dataset.source);
 
-    instance = new SimpleLightbox(`
-      <div class="modal">
-        <img src="${largeImageUrl}" alt="Large Image"/>
-      </div>
-    `);
+    instance = new SimpleLightbox(galleryItems, {
+      index,
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
 
     instance.show();
 
-    document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener('keydown', handleKeyPress);
   }
 }
 
 function handleKeyPress(event) {
-  if (event.code === "Escape" && instance) {
+  if (event.code === 'Escape' && instance) {
     instance.close();
-    document.removeEventListener("keydown", handleKeyPress);
+    document.removeEventListener('keydown', handleKeyPress);
   }
 }
 
-listGallery.addEventListener("click", handleClick);
+listGallery.addEventListener('click', handleClick);
+
+
+
+// const listGallery = document.querySelector('.gallery');
+// let instance;
+
+// function createListGallery(array) {
+//   return array
+//     .map(({ preview, description, original }) => 
+//       `<li class="gallery__item">
+//          <a class="gallery__link" href="large-image.jpg">
+//            <img
+//              class="gallery__image"
+//              src="${preview}"
+//              data-source="${original}"
+//              alt="${description}"
+//            />
+//          </a>
+//        </li>`
+//     )
+//     .join('');
+// }
+
+// listGallery.insertAdjacentHTML('beforeend', createListGallery(galleryItems));
+
+// function handleClick(event) {
+//   event.preventDefault();
+
+//   const target = event.target;
+//   if (target.classList.contains('gallery__image')) {
+//     const largeImageUrl = target.dataset.source;
+
+//     instance = new SimpleLightbox(`
+//       <div class="modal">
+//         <img src="${largeImageUrl}" alt="Large Image"/>
+//       </div>
+//     `);
+
+//     instance.show();
+
+//     document.addEventListener("keydown", handleKeyPress);
+//   }
+// }
+
+// function handleKeyPress(event) {
+//   if (event.code === "Escape" && instance) {
+//     instance.close();
+//     document.removeEventListener("keydown", handleKeyPress);
+//   }
+// }
+
+// listGallery.addEventListener("click", handleClick);
