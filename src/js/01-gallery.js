9096aaba65c4451741891e5a6e3be943cfd85832
Dmytro20777 <08dmutro11@gmail.com>
2023-11-19 09:55:54 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -36,74 +36,17 @@ function handleClick(event) {
   if (target.classList.contains('gallery__image')) {
     const index = galleryItems.findIndex(item => item.original === target.dataset.source);
 
- instance = new SimpleLightbox('.gallery', {
+ instance = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
     instance.show();
-
-    document.addEventListener('keydown', handleKeyPress);
   }
 }
 
-function handleKeyPress(event) {
-  if (event.code === 'Escape' && instance) {
-    instance.close();
-    document.removeEventListener('keydown', handleKeyPress);
-  }
-}
 
 listGallery.addEventListener('click', handleClick);
 
 
 
-// const listGallery = document.querySelector('.gallery');
-// let instance;
-
-// function createListGallery(array) {
-//   return array
-//     .map(({ preview, description, original }) => 
-//       `<li class="gallery__item">
-//          <a class="gallery__link" href="large-image.jpg">
-//            <img
-//              class="gallery__image"
-//              src="${preview}"
-//              data-source="${original}"
-//              alt="${description}"
-//            />
-//          </a>
-//        </li>`
-//     )
-//     .join('');
-// }
-
-// listGallery.insertAdjacentHTML('beforeend', createListGallery(galleryItems));
-
-// function handleClick(event) {
-//   event.preventDefault();
-
-//   const target = event.target;
-//   if (target.classList.contains('gallery__image')) {
-//     const largeImageUrl = target.dataset.source;
-
-//     instance = new SimpleLightbox(`
-//       <div class="modal">
-//         <img src="${largeImageUrl}" alt="Large Image"/>
-//       </div>
-//     `);
-
-//     instance.show();
-
-//     document.addEventListener("keydown", handleKeyPress);
-//   }
-// }
-
-// function handleKeyPress(event) {
-//   if (event.code === "Escape" && instance) {
-//     instance.close();
-//     document.removeEventListener("keydown", handleKeyPress);
-//   }
-// }
-
-// listGallery.addEventListener("click", handleClick);
